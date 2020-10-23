263353ef3a420356bdc9a5d672cb7faec48e8844
Won Young Choi <wc7qpe@virginia.edu>
2020-10-23 09:12:25 -0400

    added images to the event component

--- src/App.js
+++ src/App.js
@@ -7,23 +7,11 @@ import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
 import Card from "./components/event"
+import image_alumni_lunch from "./images/alumni_lunch.jpg";
+import image_bagels from "./images/bagels.jpg";
+import image_frisbee from "./images/frisbee.jpg";
+import image_trin from "./images/trin.jpg";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//   },
-// }));
-
-
-
-// export default function ButtonAppBar() {
-//   const classes = useStyles();
 
 
 
@@ -31,10 +19,10 @@ import Card from "./components/event"
 function App() {
 
   const data = [
-    {title: 'Frisbee on the Lawn', description: 'Play frisbee with first-years.'},
-    {title: '20% Off Bodos!', description: 'Meet at Bodos with a friend and get a 20% off coupon!'},
-    {title: 'Lunch with Alumni', description: 'Great networking opportunity to have lunch with a UVA alumni.'},
-    {title: 'Party @ Trin 3', description: 'Just be sure to wear a mask and stay 6ft apart! (also maybe just don\'t go)'},
+    {title: 'Frisbee on the Lawn', description: 'Play frisbee with first-years.', image: image_frisbee},
+    {title: '20% Off Bodos!', description: 'Meet at Bodos with a friend and get a 20% off coupon!', image: image_bagels},
+    {title: 'Lunch with Alumni', description: 'Great networking opportunity to have lunch with a UVA alumni.', image: image_alumni_lunch},
+    {title: 'Party @ Trin 3', description: 'Just be sure to wear a mask and stay 6ft apart! (also maybe just don\'t go)', image: image_trin},
   ]
 
   return (
--- src/components/event.js
+++ src/components/event.js
@@ -4,26 +4,36 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+}));
+
 
 export default function Event({props}) {
+
+  const classes = useStyles();
+
   return (
     <Card variant="outlined" style={{maxWidth: 500, margin: '50px auto 50px auto'}}>
+      <CardMedia
+        className={classes.media}
+        image={props.image}
+        title={props.title}
+      />
       <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          {props.title}
-        </Typography>
         <Typography variant="h5" component="h2">
-          {props.description}
+        {props.title}
         </Typography>
-        <Typography color="textSecondary">adjective</Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography color="textSecondary">{props.description}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small" style={{margin: "auto"}}>Learn More</Button>
       </CardActions>
     </Card>
   );
